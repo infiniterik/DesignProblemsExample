1fc6b3ed425fd7cac4deb49183109cf514a105b1
Rik Bose <88793134+infiniterik@users.noreply.github.com>
2023-03-24 16:32:41 +0000

    Fri, Mar 24, 2023, 4:32 PM +00:00

---
+++ test/JusticeTests.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class JusticeTests {
+    @Test
+    public void initializeWatchTower(){
+        WatchTower w = new WatchTower();
+    }
+
+}
